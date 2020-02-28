e28e220af03a9802d0a8152f8d5bd24aee418209
Tarık Ramazan BAŞOĞLU <32790894+trbasoglu@users.noreply.github.com>
2020-02-28 11:30:52 +0300

    Update utils_nails.py

--- utils_nails.py
+++ utils_nails.py
@@ -17,7 +17,7 @@ from IPython.display import clear_output
 
 def mean_iou(y_true, y_pred):
     yt0 = y_true[:,:,:,0]
-    yp0 = K.cast(y_pred[:,:,:,0] > 0.5, 'float32')
+    yp0 = K.cast(y_pred[:,:,:,0] > 0.8, 'float32')
     inter = tf.count_nonzero(tf.logical_and(tf.equal(yt0, 1), tf.equal(yp0, 1)))
     union = tf.count_nonzero(tf.add(yt0, yp0))
     iou = tf.where(tf.equal(union, 0), 1., tf.cast(inter/union, 'float32'))
@@ -27,8 +27,8 @@ def mean_iou(y_true, y_pred):
 class fingernailseg:
     
     epochs = 200
-    batch_size = 4
-    val_split = .25
+    batch_size = 1
+    val_split = .1
     sz = (256, 256)
     
     def __init__(self, dataset):
@@ -80,36 +80,36 @@ class fingernailseg:
         
     def create_unet(self):
         s = Input(self.sz)
-        c1 = Conv2D(16, 3, activation='relu', padding='same') (s)
-        c1 = Conv2D(16, 3, activation='relu', padding='same') (c1)
+        c1 = Conv2D(8, 3, activation='relu', padding='same') (s)
+        c1 = Conv2D(8, 3, activation='relu', padding='same') (c1)
         p1 = MaxPooling2D() (c1)
-        c2 = Conv2D(32, 3, activation='relu', padding='same') (p1)
-        c2 = Conv2D(32, 3, activation='relu', padding='same') (c2)
+        c2 = Conv2D(16, 3, activation='relu', padding='same') (p1)
+        c2 = Conv2D(16, 3, activation='relu', padding='same') (c2)
         p2 = MaxPooling2D() (c2)
-        c3 = Conv2D(64, 3, activation='relu', padding='same') (p2)
-        c3 = Conv2D(64, 3, activation='relu', padding='same') (c3)
+        c3 = Conv2D(32, 3, activation='relu', padding='same') (p2)
+        c3 = Conv2D(32, 3, activation='relu', padding='same') (c3)
         p3 = MaxPooling2D() (c3)
-        c4 = Conv2D(128, 3, activation='relu', padding='same') (p3)
-        c4 = Conv2D(128, 3, activation='relu', padding='same') (c4)
+        c4 = Conv2D(64, 3, activation='relu', padding='same') (p3)
+        c4 = Conv2D(64, 3, activation='relu', padding='same') (c4)
         p4 = MaxPooling2D() (c4)
-        c5 = Conv2D(256, 3, activation='relu', padding='same') (p4)
-        c5 = Conv2D(256, 3, activation='relu', padding='same') (c5)
-        u6 = Conv2DTranspose(128, 2, strides=(2, 2), padding='same') (c5)
+        c5 = Conv2D(128, 3, activation='relu', padding='same') (p4)
+        c5 = Conv2D(128, 3, activation='relu', padding='same') (c5)
+        u6 = Conv2DTranspose(64, 2, strides=(2, 2), padding='same') (c5)
         u6 = Concatenate(axis=3)([u6, c4])
-        c6 = Conv2D(128, 3, activation='relu', padding='same') (u6)
-        c6 = Conv2D(128, 3, activation='relu', padding='same') (c6)
-        u7 = Conv2DTranspose(64, 2, strides=(2, 2), padding='same') (c6)
+        c6 = Conv2D(64, 3, activation='relu', padding='same') (u6)
+        c6 = Conv2D(64, 3, activation='relu', padding='same') (c6)
+        u7 = Conv2DTranspose(32, 2, strides=(2, 2), padding='same') (c6)
         u7 = Concatenate(axis=3)([u7, c3])
-        c7 = Conv2D(64, 3, activation='relu', padding='same') (u7)
-        c7 = Conv2D(64, 3, activation='relu', padding='same') (c7)
-        u8 = Conv2DTranspose(32, 2, strides=(2, 2), padding='same') (c7)
+        c7 = Conv2D(32, 3, activation='relu', padding='same') (u7)
+        c7 = Conv2D(32, 3, activation='relu', padding='same') (c7)
+        u8 = Conv2DTranspose(16, 2, strides=(2, 2), padding='same') (c7)
         u8 = Concatenate(axis=3)([u8, c2])
-        c8 = Conv2D(32, 3, activation='relu', padding='same') (u8)
-        c8 = Conv2D(32, 3, activation='relu', padding='same') (c8)
-        u9 = Conv2DTranspose(16, 2, strides=(2, 2), padding='same') (c8)
+        c8 = Conv2D(16, 3, activation='relu', padding='same') (u8)
+        c8 = Conv2D(16, 3, activation='relu', padding='same') (c8)
+        u9 = Conv2DTranspose(8, 2, strides=(2, 2), padding='same') (c8)
         u9 = Concatenate(axis=3)([u9, c1])
-        c9 = Conv2D(16, 3, activation='relu', padding='same') (u9)
-        c9 = Conv2D(16, 3, activation='relu', padding='same') (c9)
+        c9 = Conv2D(8, 3, activation='relu', padding='same') (u9)
+        c9 = Conv2D(8, 3, activation='relu', padding='same') (c9)
         outputs = Conv2D(1, 1, activation='sigmoid') (c9)
         self.model = Model(inputs=[s], outputs=[outputs])
 
@@ -118,7 +118,7 @@ class fingernailseg:
     def build_callbacks(self):
         checkpointer = ModelCheckpoint(filepath='unet.h5', verbose=0, save_best_only=True, save_weights_only=False)
         stop_train = EarlyStopping(monitor='val_loss', patience=10, verbose=1)
-        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.8,
+        reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.95,
                     patience=10, min_lr=0.00001)
         callbacks = [checkpointer, reduce_lr, stop_train, PlotLearning()]
         return callbacks
